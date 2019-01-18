b3d4d015d0806cb1faeb699f7706fefe450b9c10
Omar Barazanji <ozanji345@gmail.com>
2019-01-17 22:21:53 -0600

    Added simple.cpp

---
+++ software_construction/project1/Deliverables/simple.cpp
@@ -0,0 +1,16 @@
+// This code will process an array of 10 numbers and calculates:
+// (1) a factorial value based on how many positive numbers the user inputs, and
+// (2) the value of standard deviation.
+//
+// Author: Omar Barazanji
+// Date: 1/17/2019
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is a test!" << "\n";
+	return 0;
+}
+
