a1156a2431ef651c4d1c82b9bbc6ed047618798f
Omar Barazanji <ozanji345@gmail.com>
2019-04-21 17:35:44 -0500

    updated hw5

--- software_construction/homework5/hw5_osb0002.cpp
+++ software_construction/homework5/hw5_osb0002.cpp
@@ -11,31 +11,59 @@
 #include <string>
 using namespace std;
 
-void init_list(TriviaNode Trivia);
+// structure for node
+struct TriviaNode {
+    string question;
+    string answer;
+    int points;
+    TriviaNode *next;
+};
+
+void insert_node(string question, string answer, int points, TriviaNode *head);
 /*
-*   function init_list initializes the Trivia linked list
-*   with three questions.
+*   function insert_node inserts question, answer, and points into a
+*       linked list.
 */
+
 int main() {
+    TriviaNode *head = new TriviaNode;
 
-    // sructure for node to be used for trivia.
-    struct TriviaNode {
-        string question;
-        string answer;
-        int points;
-        TriviaNode* next;
-    };
+    string ques = "Question: How long was the shortest "
+    "war on record? (Hint: how many minutes)";
+    string ans = "38";
+    int points = 100;
+    insert_node(ques, ans, points, head);
 
-    TriviaNode Trivia;
-    init_list();
+    ques = "Question: What was Bank of America's original "
+    "name? (Hint: Bank of Italy or Bank of Germany)";
+    ans = "Bank of Italy";
+    points = 50;
+    insert_node(ques, ans, points, head);
+
+    ques = "Question: What is the best-selling video game "
+    "of all time? (Hint: Minecraft or Tetris)";
+    ans = "Tetris";
+    points = 20;
+    insert_node(ques, ans, points, head);
 
     return 0;
-}
 
+}
 
 /*****************/
 /****Functions****/
 /*****************/
-void init_list(TriviaNode Trivia) {
-    
+
+void insert_node(string question, string answer, int points, TriviaNode *head){
+    TriviaNode *current = head;
+    TriviaNode *temp = new TriviaNode;
+
+    while (!(current->next == NULL)) {
+        current = current->next;
+    }
+
+    temp->question = question;
+    temp->answer = answer;
+    temp->points = points;
+    current->next = temp;
 }
