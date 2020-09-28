bf74cbb38a4da850369cdadec7d55aea6b2c7081
Omar Barazanji <ozanji345@gmail.com>
2020-09-27 21:30:22 -0500

    Lastest Scheduler

--- operating_systems/hw2/scheduler.c
+++ operating_systems/hw2/scheduler.c
@@ -23,18 +23,36 @@ void read_file(FILE *file_desc, task_struct task_list[], int *size);
 int open_file(char *name, FILE **file_descriptor);
 void print_task_list(task_struct task_list[], int size);
 int open_file(char *name, FILE **file_descriptor);
+void FCFS(task_struct task_list[], int size);
 
 
 int main(int argc, char *argv[]) {
     char *name;
+    char *type;
     FILE *fp;
     u_int count;
     task_struct task_arr[MAX_TASK_NUM];
+
+    if (argc == 1 || argc > 4) {
+        printf("Usage: command file_name [FCFS|RR|SRFT] [time_quantum]\n");
+        return 0;
+    }
     name = argv[1];
+    type = argv[2];
+    
+    printf("Scheduling Policy: %s\n", type);
+
     open_file(name, &fp);
     read_file(fp, task_arr, &count);
-    print_task_list(task_arr, count);
+
+    printf("==================================================================\n");
+
+    if (strcmp(type, "FCFS") == 0) {
+        FCFS(task_arr, count);
+    }
+
     fclose(fp);
+
     return 0;
 }
 
@@ -55,15 +73,48 @@ void read_file(FILE *file_desc, task_struct task_list[], int *size) {
 /**
  * 
  * 
+ * 
+ */
+void FCFS(task_struct task_list[], int size) {
+    int curr_pid, curr_arr_time, curr_burst;
+    int time = 0;
+    double wait_sum, turn_sum;
+    double avg_wait, avg_turn;
+    for (int i=0; i<size; i++) {
+        curr_pid = task_list[i].pid;
+        curr_arr_time = task_list[i].arrival_time;
+        curr_burst = task_list[i].burst_time;
+        int runtime = 0;
+        while (runtime < curr_burst) {
+            printf("<time %u> process %u is running\n", time, curr_pid);
+            time++;
+            runtime++;
+        }
+        turn_sum += time;
+        wait_sum += (turn_sum - curr_burst);
+        printf("<time %u> process %u is finished...\n", time, curr_pid);
+    }
+    avg_wait = wait_sum/size;
+    avg_turn = turn_sum/size;
+    printf("<time %u> All processes finshed...\n", time);
+    printf("==================================================================\n");
+    printf("Average waiting time: %f\n", avg_wait);
+    printf("Average turnaround time: %f\n", avg_turn);
+    printf("==================================================================\n");
+}
+
+/**
+ * 
+ * 
  */
 void print_task_list(task_struct task_list[], int size) {
     int i;
     printf("There are %d tasks loaded...\n", size);
     /* print task_array to check input file */
-    for (i = 0; i < size; i++) {
-        printf("Task PID: %u, Arrival Time: %u, Burst Time: %u\n", task_list[i].pid, \
-           task_list[i].arrival_time, task_list[i].burst_time);
-    }
+    // for (i = 0; i < size; i++) {
+    //     printf("Task PID: %u, Arrival Time: %u, Burst Time: %u\n", task_list[i].pid, \
+    //        task_list[i].arrival_time, task_list[i].burst_time);
+    // }
     printf("Press any key to continue ...\n");
     getchar();
 }
@@ -78,7 +129,7 @@ int open_file(char *name, FILE **file_desc) {
         return 1;
     }
 
-    printf("Open file: %s\n", name);
+    //printf("Open file: %s\n", name);
 
     return 0;
 }
