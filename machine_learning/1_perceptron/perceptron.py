7c5940fd9f0b1832645bace437a079e045af1f6a
Omar Barazanji <ozanji345@gmail.com>
2021-02-21 20:59:31 -0600

    updated perceptron

--- machine_learning/1_perceptron/perceptron.py
+++ machine_learning/1_perceptron/perceptron.py
@@ -15,25 +15,54 @@ class Perceptron:
 
     def __init__(self):
         self.bias_val = 1
-        self.w_arr = [0,1,1]
+
+        # should update to better values
+        self.w_arr = [0,0.4,0.6]
+
+        self.sep_x = []
+        self.sep_y = []
+
+    def gen_sep(self):
+        w1 = self.w_arr[1]
+        w2 = self.w_arr[2]
+        sepr_x = np.linspace(-10,10,10).tolist()
+        sepr_y = []
+        for x in sepr_x:
+            val = -(w1*x) / w2
+            sepr_y.append(val)
+        self.sep_x = sepr_x
+        self.sep_y = sepr_y
+
 
     def perceptron(self, Xin):
-        for x_n in Xin:
+        for ndx,x_n in enumerate(Xin):
             # bias
             x_0 = self.bias_val * self.w_arr[0]
             # inputs
             x_1 = x_n[0] * self.w_arr[1]
             x_2 = x_n[1] * self.w_arr[2]
             summation = x_0 + x_1 + x_2
-            print(summation)
+            # print("sum",summation)
+            self.gen_sep()
             if summation > 0:
-                if x_n[1] < 1:
+                print(ndx)
+                if x_n[1] < self.sep_y[ndx]:
                     self.w_arr[0] = self.w_arr[0] + 0.2*-1*self.bias_val
                     self.w_arr[1] = self.w_arr[1] + 0.2*-1*x_n[0]
                     self.w_arr[2] = self.w_arr[2] + 0.2*-1*x_n[1]
-            else:
-                return self.w_arr
+                else:
+                    self.w_arr[0] = self.w_arr[0] + 0.2*1*self.bias_val
+                    self.w_arr[1] = self.w_arr[1] + 0.2*1*x_n[0]
+                    self.w_arr[2] = self.w_arr[2] + 0.2*1*x_n[1]
+        return self.w_arr
 
+    def gen_data(self, dim=2):
+        X = []
+        for x in range(10):
+            point = (np.random.randint(-4,5),np.random.randint(-4,5))
+            X.append(point)
+        return X
+            
     def plot_separator(self, weights):
         # w1*x1 + w2*x2 = 0
         # solve for x2:
@@ -61,14 +90,16 @@ if __name__ == "__main__":
     X1d = [(1,0),(2,0),(3,0),(4,0)]
     X2d = [(1,2),(2,-1),(3,2),(4,-1)]
     p = Perceptron()
-    trained_weights = p.perceptron(X1d)
-    for x in range(2):
-        trained_weights = p.perceptron(X2d)
+    Xgen = p.gen_data(2)
+    print(Xgen)
+    # trained_weights = p.perceptron(X1d)
+    for x in range(10):
+        trained_weights = p.perceptron(Xgen)
     p.plot_separator(trained_weights)
-    p.plot_input(X1d, 'red', '1D (x2 = 0)')
-    p.plot_input(X2d, 'green', '2D')
+    # p.plot_input(X1d, 'red', '1D (x2 = 0)')
+    p.plot_input(Xgen, 'green', '2D')
     plt.title('Perceptron Separator for 1D and 2D data')
     plt.xlabel("x1")
     plt.ylabel("x2")
-    plt.legend(loc="upper left")
+    # plt.legend(loc="upper left")
     plt.show()
